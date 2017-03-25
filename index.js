b67dbb4c86e6bd2a7116d87e3a93151143d6928d
Pablo Rodríguez <pabloviolin8@gmail.com>
2017-03-25 14:06:38 +0100

    Finished backend auth strucutre

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
 "use strict";
 
-const express    = require("express")
+const GoogleAuth = require("google-auth-library")
+    , express    = require("express")
     , bodyParser = require("body-parser");
+
+const GOOGLE_API_CLIENT_ID =
+    "922325936396-gb9hvho675353pr218mbjvtvbuvnq649.apps.googleusercontent.com";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const auth = new GoogleAuth;
+const gClient = new auth.OAuth2(GOOGLE_API_CLIENT_ID, "", "");
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
+app.post("/login", async (req, res) => {
+    if (!req.body.idToken) {
+        return res.status(400).send(JSON.stringify({
+            error: "The idToken is missing."
+        }));
+    }
 
-    res.status(200).send("Hola mundo!!")
+    try {
 
-});
+        let user = await getUserFromToken(req.body.idToken);
+        console.log("User authenticated:", user);
+        res.status(200).send();
 
-app.post("/login", (req, res) => {
+    } catch(e) {
 
-    res.status(200).send(JSON.stringify(req.body));
+        res.status(401).send(JSON.stringify({
+            error: "Google authentication failed."
+        }));
 
+    }
 });
 
 app.listen(PORT, () => console.log(`Listening in port ${PORT}`));
+
+const getUserFromToken = token => new Promise((fulfill, reject) => {
+
+    gClient.verifyIdToken(token, GOOGLE_API_CLIENT_ID, (e, login) => {
+
+        if (e) {
+            return reject(e);
+        }
+
+        const payload = login.getPayload();
+        return fulfill({
+            name: payload.name,
+            email: payload.email,
+            picture: payload.picture,
+            googleId: payload.sub
+        });
+
+    });
+
+});
